a29ea4f27217433b0adca12dbf1c1935c6896a8a
David Wolgemuth <davidjwolgemuth@gmail.com>
2016-03-19 10:59:54 -0700

    Plot Line example written, not visible on map yet

--- Seattle Trails/ViewController.swift
+++ Seattle Trails/ViewController.swift
@@ -17,7 +17,15 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        plotPoint()
+        let coordinate = CLLocationCoordinate2D(latitude: 47.6190648, longitude: -122.3391903)
+        let region = MKCoordinateRegionMakeWithDistance(coordinate, 25000, 25000)
+        mapView.setRegion(region, animated: true)
+        plotLine()
+    }
+    
+    @IBAction func navButtonPressed(sender: UIButton)
+    {
+        
     }
     
     func plotPoint()
@@ -30,6 +38,23 @@ class ViewController: UIViewController
         
         mapView.addAnnotation(annotation)
         
+    }
+    func plotLine()
+    {
+        // Not Showing Yet
+
+        let coordinateA = CLLocationCoordinate2D(latitude: 47.58144035069734, longitude: -122.38291159311778)
+        let coordinateB = CLLocationCoordinate2D(latitude: 60.08144035069734, longitude: -100.98291159311778)
+        let coordinateC = CLLocationCoordinate2D(latitude: 60.08144035069734, longitude: -100.98291159311778)
+        let coordinateD = CLLocationCoordinate2D(latitude: 60.08144035069734, longitude: -100.98291159311778)
+        
+        var coordinates: [CLLocationCoordinate2D] = [CLLocationCoordinate2D]()
+        coordinates.append(coordinateA)
+        coordinates.append(coordinateB)
+        coordinates.append(coordinateC)
+        coordinates.append(coordinateD)
+        let line = MKPolyline(coordinates: &coordinates, count: 4)
+        mapView.addOverlay(line)
     }
 }
 
