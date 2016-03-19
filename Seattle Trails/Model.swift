5f4b4c4457d30a993c1edf87a0c2361e889e0a18
Theodore Abshire <TheodoreAbshire@Gmail.com>
2016-03-19 11:09:04 -0700

    Added the get nearest and get in area.

--- Seattle Trails/Model.swift
+++ Seattle Trails/Model.swift
@@ -21,97 +21,200 @@ class Trail
 	
 	//utility functions
 	var center:CGPoint
+	{
+		var c = CGPoint(x: 0, y: 0)
+		for point in points
 		{
-			var c = CGPoint(x: 0, y: 0)
-			for point in points
-			{
-				c.x += point.x
-				c.y += point.y
-			}
-			c.x /= CGFloat(points.count)
-			c.y /= CGFloat(points.count)
-			return c
+			c.x += point.x
+			c.y += point.y
+		}
+		c.x /= CGFloat(points.count)
+		c.y /= CGFloat(points.count)
+		return c
 	}
 }
 
 
 let appToken = "o9zqUXd72sDpc0BWNR45Fc1TH"
+let timeoutPeriod:Double = 3600
+
+//info cache
+var localCacheInner:[Trail]?
 
 class socrataService
 {
 	class func getNearestTrail(nearestTo:CGPoint, returnClosure:((Trail?)->()))
 	{
-		//TODO: whatever
-//		getAllTrails(
-//			{ (all:[Trail]) -> () in
-//				returnClosure(all[0])
-//		})
+		getAllTrails()
+		{ (trails) in
+			if let trails = trails
+			{
+				if trails.count == 0
+				{
+					returnClosure(nil)
+				}
+				else
+				{
+					var closest = trails[0]
+					for trail in trails
+					{
+						let center = trail.center
+						let xDif = center.x - nearestTo.x
+						let yDif = center.y - nearestTo.y
+						let distance = xDif*xDif + yDif*yDif
+						
+						let oldCenter = closest.center
+						let oldXDif = oldCenter.x - nearestTo.x
+						let oldYDif = oldCenter.y - nearestTo.y
+						let oldDistance = oldXDif*oldXDif + oldYDif*oldYDif
+						
+						if distance < oldDistance
+						{
+							closest = trail
+						}
+					}
+					returnClosure(closest)
+				}
+			}
+			else
+			{
+				returnClosure(nil)
+			}
+		}
 	}
 	
 	class func getTrailsInArea(area:CGRect, returnClosure:(([Trail]?)->()))
 	{
-		//TODO: whatever
-//		getAllTrails(returnClosure)
+		getAllTrails()
+		{ (trails) in
+			if let trails = trails
+			{
+				var validTrails = [Trail]()
+				for trail in trails
+				{
+					for point in trail.points
+					{
+						if area.contains(point)
+						{
+							validTrails.append(trail)
+							break
+						}
+					}
+				}
+				returnClosure(validTrails)
+			}
+			else
+			{
+				returnClosure(nil)
+			}
+		}
 	}
 	
 	class func getAllTrails(returnClosure:(([Trail]?)->()))
 	{
-		//do network calls
-		doRequest(returnClosure)
+		var tryToReplace = false
+		if let date = NSUserDefaults.standardUserDefaults().objectForKey("storedDate") as? NSDate
+		{
+			let currentDate = NSDate()
+			let difference = currentDate.timeIntervalSinceDate(date)
+			if difference > timeoutPeriod
+			{
+				tryToReplace = true
+			}
+		}
+		
+		
+		if (localCache != nil && !tryToReplace)
+		{
+			//return the local cache
+			returnClosure(localCache);
+		}
+		else
+		{
+			//do network calls
+			doRequest()
+			{ (trails, json) in
+				if let trails = trails, let json = json
+				{
+					//cache it
+					localCacheInner = trails
+					returnClosure(trails)
+					
+					//store it in user defaults too
+					NSUserDefaults.standardUserDefaults().setObject(json, forKey: "storedJSON")
+					NSUserDefaults.standardUserDefaults().setObject(NSDate(), forKey: "storedDate")
+					
+					//save the JSON
+					returnClosure(trails)
+				}
+				else
+				{
+					//it failed to load, so return the cache whether or not it's nil
+					returnClosure(localCache)
+				}
+			}
+		}
 	}
-	
-	private class func serialize(data:NSData) -> [Trail]?
+	private class func serializeInner(json:[[String : AnyObject]]) -> [Trail]
+	{
+		var trails = [Trail]()
+		for dict in json
+		{
+			//TODO: parse this JSON dictionary
+			//into a trail
+			let canopy = dict["canopy"] as? String ?? "Unknown"
+			let condition = dict["condition"] as? String ?? "Unknown"
+			let gradeType = dict["grade_type"] as? String ?? "Unknown"
+			let surfaceType = dict["surface_ty"] as? String ?? "Unknown"
+			if let name = dict["pma_name"] as? String, let length = dict["gis_length"] as? String, let geom = dict["the_geom"] as? [String:AnyObject]
+			{
+				let trail = Trail()
+				trail.canopy = canopy
+				trail.condition = condition
+				trail.gradeType = gradeType
+				trail.surfaceType = surfaceType
+				trail.name = name
+				trail.length = (length as NSString).floatValue
+				
+				if let points = geom["coordinates"] as? [[Int]]
+				{
+					for point in points
+					{
+						trail.points.append(CGPoint(x: point[0], y: point[1]))
+					}
+					trails.append(trail)
+				}
+				else
+				{
+					NSLog("ERROR: failed to load points on trail " + name + "!");
+				}
+				
+			}
+			else
+			{
+				NSLog("ERROR: failed to load trail!");
+			}
+		}
+		
+		return trails
+	}
+	private class func serialize(data:NSData) -> ([Trail]?, [[String : AnyObject]]?)
 	{
 		do
 		{
 			if let json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as? [[String : AnyObject]]
 			{
-				var trails = [Trail]()
-				for dict in json
-				{
-					//TODO: parse this JSON dictionary
-					//into a trail
-					if let canopy = dict["canopy"] as? String, let condition = dict["condition"] as? String, let gradeType = dict["grade_type"] as? String, let surfaceType = dict["surface_ty"] as? String, let name = dict["pma_name"] as? String, let length = dict["gis_length"] as? String, let geom = dict["the_geom"] as? [String:AnyObject]
-					{
-						let trail = Trail()
-						trail.canopy = canopy
-						trail.condition = condition
-						trail.gradeType = gradeType
-						trail.surfaceType = surfaceType
-						trail.name = name
-						trail.length = (length as NSString).floatValue
-						
-						if let points = geom["coordinates"] as? [[Int]]
-						{
-							for point in points
-							{
-								trail.points.append(CGPoint(x: point[0], y: point[1]))
-							}
-							trails.append(trail)
-						}
-						else
-						{
-							NSLog("ERROR: failed to load points on trail " + name + "!");
-						}
-						
-					}
-					else
-					{
-						NSLog("ERROR: failed to load trail!");
-					}
-				}
-				
-				return trails
+				return (serializeInner(json), json)
 			}
 		}
 		catch let error
 		{
 		}
 		NSLog("ERROR: failed to load trails!");
-		return nil
+		return (nil, nil)
 	}
 	
-	private class func doRequest(completion:([Trail]?)->())
+	private class func doRequest(completion:([Trail]?, [[String : AnyObject]]?)->())
 	{
 		let urlString = "https://data.seattle.gov/resource/vwtx-gvpm.json?$$app_token=" + appToken
 		if let url = NSURL(string: urlString)
@@ -133,15 +236,22 @@ class socrataService
 				{
 					//TODO: note there still might be a problem (ie check to see if there's a "error" argument!)
 					
-					if let serialized = serialize(data)
+					let result = serialize(data)
+					NSOperationQueue.mainQueue().addOperationWithBlock()
 					{
-						NSOperationQueue.mainQueue().addOperationWithBlock()
-						{
-							completion(serialized)
-						}
+						completion(result.0, result.1)
 					}
 				}
 			}).resume()
 		}
+	}
+	
+	private class var localCache:[Trail]?
+	{
+		if localCacheInner == nil, let json = NSUserDefaults.standardUserDefaults().objectForKey("storedJSON") as? [[String : AnyObject]]
+		{
+			localCacheInner = socrataService.serializeInner(json)
+		}
+		return localCacheInner
 	}
 }
