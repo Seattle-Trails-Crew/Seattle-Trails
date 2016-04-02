7a104f1c9ae8b31e35a15901d61d7d8e10dcd305
Theodore Abshire <TheodoreAbshire@Gmail.com>
2016-04-01 17:06:46 -0700

    Now the AFNetworking works.

---
+++ Seattle Trails/AFNetworkingBridge.h
@@ -0,0 +1,9 @@
+//
+//  AFNetworkingBridge.h
+//  Seattle Trails
+//
+//  Created by Theodore Abshire on 4/1/16.
+//  Copyright © 2016 seatrails. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
