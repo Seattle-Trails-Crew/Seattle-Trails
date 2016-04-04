d748518eb7b84a1d28b795f1771c5b562df7c171
EricMentele <diviningsage@gmail.com>
2016-04-03 22:35:38 -0400

    Slight change to async calls for matching styling.

--- Seattle Trails/AlertViewsController.swift
+++ Seattle Trails/AlertViewsController.swift
@@ -16,7 +16,8 @@ class AlertViews {
         let issueView = UIAlertController(title: "Report Issue", message: "You must be on site at a trail or park to use this feature and report an issue. Thank you for helping us document park problems for service.", preferredStyle: .Alert)
         let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
         issueView.addAction(okButton)
-        dispatch_async(dispatch_get_main_queue()) {
+        dispatch_async(dispatch_get_main_queue())
+        {
             sender.presentViewController(issueView, animated: true, completion: nil)
         }
     }
@@ -25,28 +26,36 @@ class AlertViews {
         let issueErrorView = UIAlertController(title: "Report Failure", message: "Your device is currently unable to send email. Please check your email settings and network connection then try again. Thank you.", preferredStyle: .Alert)
         let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
         issueErrorView.addAction(okButton)
-        dispatch_async(dispatch_get_main_queue()) {
+        dispatch_async(dispatch_get_main_queue())
+        {
             sender.presentViewController(issueErrorView, animated: true, completion: nil)
         }
     }
     
     // MARK: Option Alert Views
+    /**
+     An alert controller allowing the user to pick their photo library or the camera as an image source.
+     
+     - parameter sender: <#sender description#>
+     */
     class func presentImageSourceSelectionView (sender sender: ViewController) {
         // Present image picker options.
         let actionSheet = UIAlertController(title: "Image Source", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
         
         let cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default)
         { (action) in
-            dispatch_async(dispatch_get_main_queue(), {
+            dispatch_async(dispatch_get_main_queue())
+            {
                 sender.presentIssueImagePickerWithSourceType(UIImagePickerControllerSourceType.Camera)
-            })
+            }
         }
         
         let libraryAction = UIAlertAction(title: "Photo Library", style: UIAlertActionStyle.Default)
         { (action) in
-            dispatch_async(dispatch_get_main_queue(), {
+            dispatch_async(dispatch_get_main_queue())
+            {
                 sender.presentIssueImagePickerWithSourceType(UIImagePickerControllerSourceType.PhotoLibrary)
-            })
+            }
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
@@ -55,9 +64,10 @@ class AlertViews {
         actionSheet.addAction(libraryAction)
         actionSheet.addAction(cancelAction)
         
-        dispatch_async(dispatch_get_main_queue(), {
+        dispatch_async(dispatch_get_main_queue())
+        {
             sender.presentViewController(actionSheet, animated: true, completion: nil)
-        })
+        }
     }
     
     class func presentIssueReportImageOptionView(sender sender: ViewController, parkName: String) {
@@ -69,7 +79,8 @@ class AlertViews {
         
         fileIssueView.addAction(okButton)
         
-        dispatch_async(dispatch_get_main_queue()) {
+        dispatch_async(dispatch_get_main_queue())
+        {
             sender.presentViewController(fileIssueView, animated: true, completion: nil)
         }
     }
